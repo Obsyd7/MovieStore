ed219a22926a1a2c859aab5834913eecd5fd3932
Patryk Pater <paterp989@gmail.com>
2018-10-12 09:09:34 +0200

    RestrictAccess

--- MovieStore/App_Start/BundleConfig.cs
+++ MovieStore/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace MovieStore
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/scripts/bootbox.js,"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,13 +21,10 @@ namespace MovieStore
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/content/datatables/css/datatables.bootstrap.css"));
         }
     }
 }
--- MovieStore/Controllers/HomeController.cs
+++ MovieStore/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 
 namespace MovieStore.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
